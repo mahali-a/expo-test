641f23e0f8051775ec1d1072b1590cfad5ed8ca1
Mahali Asmah <mahaliasmah@gmail.com>
2024-09-05 17:54:04 -0400

    test

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,36 @@
-import { createRequestHandler } from "@expo/server/adapter/vercel";
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 
-import path from "path";
+const { createRequestHandler } = require("@expo/server/adapter/express");
+const compression = require("compression");
+const express = require("express");
+const morgan = require("morgan");
 
-module.exports = createRequestHandler({
-  build: path.join(__dirname, "../dist/server"),
+const path = require("path");
+
+const SERVER_BUILD_DIR = path.join(process.cwd(), "dist/server");
+
+const app = express();
+
+app.use(compression());
+
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
+
+process.env.NODE_ENV = "production";
+
+app.use(morgan("tiny"));
+
+app.all(
+  "*",
+  createRequestHandler({
+    build: SERVER_BUILD_DIR,
+  }),
+);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
+
+module.exports = app;
